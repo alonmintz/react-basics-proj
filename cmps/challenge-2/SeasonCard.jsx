bd7d351f24e20038ec7eaaafe6878a98b6ce3ded
alonmintz <92505398+alonmintz@users.noreply.github.com>
2025-02-03 23:44:49 +0200

    paths fix

--- cmps/challenge-2/SeasonCard.jsx
+++ cmps/challenge-2/SeasonCard.jsx
@@ -33,7 +33,7 @@ export function SeasonCard() {
 
   function getImgUrl() {
     if (!dateDetails.season) return;
-    return `assets/img/challenge-2/${dateDetails.season}.png`;
+    return `react-basic-proj/assets/img/challenge-2/${dateDetails.season}.png`;
   }
 
   const darkClass = isDark ? "dark" : "";
